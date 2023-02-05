1617cd4ab7991b05201239839ac374227788af40
Victor Fernando <victorfernando.sis@gmail.com>
2023-02-05 01:45:24 -0300

    CRUD

--- src/pages/NewStudent/index.js
+++ src/pages/NewStudent/index.js
@@ -1,100 +1,98 @@
-import React , {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
-import {Link,useHistory , useParams} from 'react-router-dom';
-import {FiCornerDownLeft, FiUserPlus } from 'react-icons/fi';
-// import api from '../../services/api';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { FiCornerDownLeft, FiUserPlus } from 'react-icons/fi';
+import api from '../../services/api';
 
-export default function NewStudent(){
+export default function NewStudent() {
 
-   const [id,setId]= useState(null);
+   const [id, setId] = useState(null);
    const [name, setName] = useState('');
    const [email, setEmail] = useState('');
    const [age, setAge] = useState(0);
 
-    const {studentId} = useParams();
-//     const history = useHistory();
+   const { studentId } = useParams();
+   const navigate = useNavigate();
 
-//     const token = localStorage.getItem('token');
-//     const authorization = {
-//       headers : {
-//         Authorization : `Bearer ${token}`
-//       }
-//     }  
+   const token = localStorage.getItem('token');
+   const authorization = {
+      headers: {
+         Authorization: `Bearer ${token}`
+      }
+   }
 
-//     useEffect(()=>{
-//        if(studentId === '0')
-//          return;
-//        else
-//          loadStudent();  
-//     }, studentId)
+   useEffect(() => {
+      if (studentId === '0')
+         return;
+      else
+         loadStudent();
+   }, [])
 
-//     async function loadStudent(){
-//        try{
+   async function loadStudent() {
+      try {
 
-//          const response = await api.get(`api/students/${studentId}`,authorization) ;
+         const response = await api.get(`api/students/${studentId}`, authorization);
 
-//          setId(response.data.id);
-//          setName(response.data.name);
-//          setEmail(response.data.email);
-//          setAge(response.data.age);
-//        }catch(error){
-//          alert('Erro ao recuperar o student ' + error);
-//          history.push('/students');
-//        }
-//     }
+         setId(response.data.studentId);
+         setName(response.data.name);
+         setEmail(response.data.email);
+         setAge(response.data.age);
+      } catch (error) {
+         alert('Error while retriving student ' + error);
+         navigate('students');
+      }
+   }
 
-    async function saveOrUpdate(event) {
-//          event.preventDefault();
+   async function saveOrUpdate(event) {
+      event.preventDefault();
 
-//          const data = {
-//             name,
-//             email,
-//             age
-//          }
+      const data = {
+         name,
+         email,
+         age
+      }
 
-//          try{
-//            if(studentId==='0')
-//            {
-//               await api.post('api/students',data,authorization);
-//            }
-//            else
-//            {
-//               data.id= id;
-//               await api.put(`api/students/${id}`,data,authorization)
-//            }
-//          }catch(error){
-//             alert('Erro ao gravar student ' + error);
-//          }
-//          history.push('/students');
-    }
+      try {
+         if (studentId === '0') {
+            await api.post('api/students', data, authorization);
+         }
+         else {
+            data.studentId = id;
+            await api.put(`api/students/${id}`, data, authorization)
+         }
+      } catch (error) {
+         alert('Error saving student ' + error);
+      }
+      navigate('/students');
+   }
 
-    return(
-        <div className="new-student-container">
-           <div className="content">
-           <section className="form"><FiUserPlus size="105" color="#17202a"/>
-             <h1>{studentId === '0'? 'Add New Student' : 'Atualizar Student'}</h1>
+   return (
+      <div className="new-student-container">
+         <div className="content">
+            <section className="form"><FiUserPlus size="105" color="#17202a" />
+               <h1>{studentId === '0' ? 'Add New Student' : 'Update Student'}</h1>
                <Link className="back-link" to="/students">
-                <FiCornerDownLeft size="25" color="#17202a"/> Retornar
+                  <FiCornerDownLeft size="25" color="#17202a" /> Go back
                </Link>
             </section>
-            
+
             <form onSubmit={saveOrUpdate}>
-               <input  placeholder="Name" 
+               <input placeholder="Name"
                   value={name}
-                  onChange= {e=> setName(e.target.value)}
+                  onChange={e => setName(e.target.value)}
                />
-               <input  placeholder="Email" 
-                value={email}
-                onChange={e => setEmail(e.target.value)}
+               <input placeholder="Email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
                />
-               <input  placeholder="Age" 
+               <input placeholder="Age"
                   value={age}
                   onChange={e => setAge(e.target.value)}
                />
-                  <button className="button" type="submit">{studentId === '0'? 'Add ' : 'Atualizar '}</button>
+               <button className="button" type="submit">{studentId === '0' ? 'Add ' : 'Update '}</button>
             </form>
 
-           </div>
-        </div>
-    );
+         </div>
+      </div>
+   );
 }
--- src/pages/Student/index.js
+++ src/pages/Student/index.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './styles.css';
-// import api from '../../services/api';
+import api from '../../services/api';
 
 
 import cadastro1 from '../../assets/cadastro1.png';
@@ -10,130 +10,129 @@ import { FiXCircle, FiEdit, FiUserX } from 'react-icons/fi';
 export default function Student() {
 
     //  //filtrar dados
-     const [searchInput,setSearchInput]  = useState('');
-     const [filtro, setFiltro] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
+    const [filter, setFilter] = useState([]);
 
-     const [students, setStudents] = useState([]);
+    const [students, setStudents] = useState([]);
 
-     const email = localStorage.getItem('email');
-     const token = localStorage.getItem('token');
+    const email = localStorage.getItem('email');
+    const token = localStorage.getItem('token');
 
-    //  const history = useHistory();
-   
-    //  const authorization = {
-    //      headers : {
-    //        Authorization : `Bearer ${token}`
-    //      }
-    //  }
+    const navigate = useNavigate();
 
-    //  const searchStudents = (searchValue) => {
-    //   setSearchInput(searchValue);
-    //   if (searchInput !== '') {
-    //       const dadosFiltrados = students.filter((item) => {
-    //           return Object.values(item).join('').toLowerCase()
-    //           .includes(searchInput.toLowerCase())
-    //       });
-    //       setFiltro(dadosFiltrados);
-    //   }
-    //   else{
-    //       setFiltro(students);
-    //   }
-    // }
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
 
-    //  useEffect( ()=> {
-    //    api.get('api/students',authorization).then(
-    //      response=> {setStudents(response.data);
-    //     }, token)
-    //  })
+    const searchStudents = (searchValue) => {
+        setSearchInput(searchValue);
+        if (searchInput !== '') {
+            const filtered = students.filter((item) => {
+                return Object.values(item).join('').toLowerCase()
+                    .includes(searchInput.toLowerCase())
+            });
+            setFilter(filtered);
+        }
+        else {
+            setFilter(students);
+        }
+    }
 
-     async function logout(){
-    //    try{
-    //       localStorage.clear();
-    //       localStorage.setItem('token','');
-    //       authorization.headers ='';
-    //       history.push('/'); 
-    //    }catch(err){
-    //     alert('Não foi possível fazer o logout' + err);
-    //    }
-     }
+    useEffect(() => {
+        api.get('api/students', authorization).then(
+            response => {
+                setStudents(response.data);
+            }, token)
+    }, [])
 
-     async function editStudent(id){
-    //    try{
-    //      history.push(`student/new/${id}`);
-    //    }catch(error){
-    //     alert('Não foi possível editar o student')
-    //    }
-     }
+    async function logout() {
+        try {
+            localStorage.clear();
+            localStorage.setItem('token', '');
+            authorization.headers = '';
+            navigate('/');
+        } catch (err) {
+            alert('Erro while trying to logout: ' + err);
+        }
+    }
 
-     async function deleteStudent(id){
-    //    try{
-    //       if(window.confirm('Deseja deletar o student de id = ' + id + ' ?'))
-    //       {
-    //             await api.delete(`api/students/${id}`, authorization);
-    //             setStudents(students.filter(student => student.id !== id));
-    //       }
-    //    }catch(error){
-    //     alert('Não foi possível excluir o student')
-    //    }
-     }
+    async function editStudent(id) {
+        try {
+            navigate(`new/${id}`);
+        } catch (error) {
+            alert(`Couldn't find student`)
+        }
+    }
+
+    async function deleteStudent(id) {
+        try {
+            if (window.confirm('Would you like to delete id = ' + id + ' ?')) {
+                await api.delete(`api/students/${id}`, authorization);
+                setStudents(students.filter(student => student.studentId !== id));
+            }
+        } catch (error) {
+            alert('Não foi possível excluir o student')
+        }
+    }
 
 
     return (
         <div className="student-container">
             <header>
-               <img src={cadastro1} alt="Registration" />
-               <span>Bem-Vindo, <strong>{'email'}</strong>!</span>
-               <Link className="button" to="new/0">Novo Student</Link>
+                <img src={cadastro1} alt="Registration" />
+                <span>Welcome, <strong>{email}</strong>!</span>
+                <Link className="button" to="new/0">New Student</Link>
 
-               <button onClick={'logout'} type="button">
-                   <FiXCircle size={35}  color="#17202a" />
-               </button>
-               {/* <a onClick={'logout'}><FiXCircle size={35}  color="#17202a" /> </a> */}
+                <button onClick={logout} type="button">
+                    <FiXCircle size={35} color="#17202a" />
+                </button>
             </header>
 
             <form>
-              <input type='text'
-               placeholder='Filtrar por nome...'
-                // onChange={(e) => searchStudents(e.target.value)} 
-              />
+                <input type='text'
+                    placeholder='Filter by name...'
+                    onChange={(e) => searchStudents(e.target.value)}
+                />
             </form>
 
-            <h1>Relação de Students</h1>
+            <h1>Students List</h1>
             {searchInput.length > 1 ? (
-               <ul> 
-               {filtro.map(student => (
-                    <li key={student.Id}>
-                        <b>Nome:</b>{student.nome}<br/><br/>
-                        <b>Email:</b>{student.email}<br/><br/>
-                        <b>Idade:</b>{student.idade}<br/><br/>
-                        <button onClick={()=> editStudent(student.id)} type="button">
-                            <FiEdit size="25" color="#17202a" />
-                        </button>
-                        <button type="button" onClick= {()=> deleteStudent(student.id)}> 
-                            <FiUserX size="25" color="#17202a" />
-                        </button>
-                    </li>
-                ))}
-              </ul>
-              ) : (
-            <ul>
-               {students.map(student=>(
-                 <li key={student.id}>
-                  <b>Nome:</b>{student.nome}<br/><br/>
-                  <b>Email:</b>{student.email}<br/><br/>
-                  <b>Idade:</b>{student.idade}<br/><br/>
+                <ul>
+                    {filter.map(student => (
+                        <li key={student.studentId}>
+                            <b>Name:</b>{student.name}<br /><br />
+                            <b>Email:</b>{student.email}<br /><br />
+                            <b>Age:</b>{student.age}<br /><br />
+                            <button onClick={() => editStudent(student.studentId)} type="button">
+                                <FiEdit size="25" color="#17202a" />
+                            </button>
+                            <button type="button" onClick={() => deleteStudent(student.studentId)}>
+                                <FiUserX size="25" color="#17202a" />
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <ul>
+                    {students.map(student => (
+                        <li key={student.studentId}>
+                            <b>Name:</b>{student.name}<br /><br />
+                            <b>Email:</b>{student.email}<br /><br />
+                            <b>Age:</b>{student.age}<br /><br />
 
-                 <button onClick={()=> editStudent(student.id)} type="button">
-                     <FiEdit size="25" color="#17202a" />
-                 </button>
+                            <button onClick={() => editStudent(student.studentId)} type="button">
+                                <FiEdit size="25" color="#17202a" />
+                            </button>
 
-                 <button type="button" onClick= {()=> deleteStudent(student.id)}> 
-                         <FiUserX size="25" color="#17202a" />
-                   </button>
-               </li>
-                ))}
-            </ul>
-           )}
+                            <button type="button" onClick={() => deleteStudent(student.studentId)}>
+                                <FiUserX size="25" color="#17202a" />
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
-     );
+    );
 }
